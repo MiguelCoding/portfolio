70dd9f24db64e4789ceb42d053baa67540d47191
MiguelCoding <miguelruizjax@gmail.com>
2023-04-17 23:59:14 -0400

    testing the login and user section

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,9 @@
+// assist the import of components to the outside.
+
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Header } from "./header/Header";
 export { default as Projects } from "./projects/Projects";
 export { default as Download } from "./download/Download";
 export { default as Socials } from "./socials/Socials";
 export { default as Footer } from "./footer/Footer";
+export { default as Login } from "./login/Login";
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,29 +1,37 @@
-
-
-
+import React, { useState } from 'react';
+import './Login.css';
 
 
 
 
 const Login = () => {
-  return (
-    <div className="login-container">
-      <div className="login">
-        <h1>Login</h1>
-        <form action="">
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" placeholder="Enter email" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter password" />
-          </div>
-          <button className="btn btn-block">Login</button>
-        </form>
-      </div>
-    </div>
-  );
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+    // Here you would typically send the username and password to your backend server for authentication
+  };
+    return (
+        <div className="login-container">
+        <div className="login">
+            <h1>Login</h1>
+            <form action="">
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" placeholder="Enter email" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" placeholder="Enter password" />
+            </div>
+            <button className="btn btn-block">Login</button>
+            </form>
+        </div>
+        </div>
+    );
 }
 
 export default Login;
