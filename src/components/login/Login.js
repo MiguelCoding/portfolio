db983c21d8c141c7270c763877837ccf8a05da7a
MiguelCoding <miguelruizjax@gmail.com>
2023-05-17 22:44:57 -0400

    changes to header and login for phone. noLoginpage

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -15,22 +15,25 @@ const Login = () => {
     // Here you would typically send the username and password to your backend server for authentication
   };
     return (
-        <div className="login-container">
-        <div className="login">
-            <h1>Login</h1>
-            <form action="">
-            <div className="form-control">
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" placeholder="Enter email" />
+        //this is important such that the navbar can scroll to this section
+        <section id="login"> 
+            <div className="login-container">
+                <div className="login">
+                    <h1>Login</h1>
+                    <form action="">
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" id="email" placeholder="Enter email" />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" placeholder="Enter password" />
+                    </div>
+                    <button className="btn btn-block">Login</button>
+                    </form>
+                </div>
             </div>
-            <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" placeholder="Enter password" />
-            </div>
-            <button className="btn btn-block">Login</button>
-            </form>
-        </div>
-        </div>
+        </section>
     );
 }
 
